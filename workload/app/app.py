889edef96ea95d01e2735b595933de6fb9265306
Matheus Cintra <matheus.goncalves@alumni.usp.br>
2023-09-30 18:19:15 -0300

    adicionado teste no app python

--- workload/app/app.py
+++ workload/app/app.py
@@ -1,10 +1,15 @@
 from flask import Flask
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+    @app.route("/")
+    def hello_world():
+        return "<p>Hello, World!</p>"
+
+    return app
 
 if __name__ == "__main__":
+    app = create_app()
+
     app.run(debug=True)
---
+++ workload/app/tests/test_hello_world.py
@@ -0,0 +1,4 @@
+def test_request_example(client):
+    response = client.get("/")
+    assert b"<p>Hello, World!</p>" in response.data
+
