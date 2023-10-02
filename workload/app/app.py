cb32857af6ac06749f71f60bf2e10b5f90db9431
Matheus Cintra <matheus.goncalves@alumni.usp.br>
2023-10-01 23:00:36 -0300

    adicionado metricas no app

--- workload/app/app.py
+++ workload/app/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
 
 def create_app():
     app = Flask(__name__)
+    PrometheusMetrics(app)
 
     @app.route("/")
     def hello_world():
